284f302fb81285da62792372a32be0f3ccde9839
Mikhail Ushakov <mikhail.a.ushakov@gmail.com>
2025-03-27 04:07:59 +0100

    - Implemented single-source shortest path algorithm for unweighted graphs "bfs_sp" - implemented Floyd Warshall all-source shortest path algorithm - Re-organized inclusions for memtrs stdlib - Added extra data structure to represent matrices as lctrs terms - Added extra array functions: fillArray - Added extra matrix functions: fillMatrix, fillMatrixDiag, matrixToTerm - Fixed graphToMatrix function

--- app/src/main/java/charlie/parser/Preprocessor.java
+++ app/src/main/java/charlie/parser/Preprocessor.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Preprocessor {
 
@@ -11,19 +13,26 @@ public class Preprocessor {
 
   private final Path source;
 
+  private final Set<Path> includedFiles;
+
   Preprocessor(String filename) {
     this.source = Path.of(filename);
+    this.includedFiles = new HashSet<>();
   }
 
   private void unpack(Path source, Path dest) throws IOException {
+    includedFiles.add(source);
     Files.lines(source)
       .forEach(line -> {
         if (line.startsWith(INCLUDE_KEY)) {
           String subSource = line.substring(INCLUDE_KEY.length()).trim();
           try {
-            unpack(source.getParent().resolve(Path.of(subSource)), dest);
+            Path inclusion = source.getParent().resolve(Path.of(subSource)).normalize();
+            if (!includedFiles.contains(inclusion)) {
+              unpack(inclusion, dest);
+            }
           } catch (IOException e) {
-            throw new RuntimeException("Preprocessor is unable to resolve the file inclusion: " + subSource);
+            throw new RuntimeException("Preprocessor is unable to resolve the file inclusion: " + subSource + ", for: " + source);
           }
         } else {
           try {
@@ -36,9 +45,10 @@ public class Preprocessor {
   }
 
   public String preprocess() {
+    includedFiles.clear();
     Path coraTmpFile = null;
     try {
-      coraTmpFile = Files.createTempFile("cora_",
+      coraTmpFile = Files.createTempFile("cora_preprocessor_",
           source.toString().substring(source.toString().lastIndexOf(".")));
       unpack(source, coraTmpFile);
       return coraTmpFile.toAbsolutePath().toString();
